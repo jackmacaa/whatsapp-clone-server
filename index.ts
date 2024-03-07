ef2a9aab34b451adf0d6da8b14966295550f4d2a
jack.mcloughlin <jackmaca@msn.com>
2024-03-07 15:58:45 +1030

    Added db to server and chats API created

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log('hello world')
+import express from 'express';
+import { chats } from './db';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/_ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.get('/chats', (_req, res) => {
+  res.json(chats);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
